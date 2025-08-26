df47e0985dcd2edb2c971aaa78671685b7732302
Luiz Claudio- Dev <luizclaudio5423@gmail.com>
2025-08-26 17:33:51 -0300

    Atualizar o create-pix.js
    código novo

--- create-pix.js
+++ create-pix.js
@@ -1,3 +1,5 @@
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET');
 import { QrCodePix } from 'qrcode-pix';
 
 export default async function handler(req, res) {
