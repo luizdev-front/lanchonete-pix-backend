6c042f23f8d2961f98c5d10e4e28e894a6f15eae
Luiz Claudio- Dev <luizclaudio5423@gmail.com>
2025-08-26 18:16:01 -0300

    Create create-pix.js
    Adiciona endpoint create-pix

---
+++ api/create-pix.js
@@ -0,0 +1,24 @@
+import { QrCodePix } from 'qrcode-pix';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  const { amount, name, message } = req.query;
+
+  const qrCodePix = QrCodePix({
+    version: '01',
+    key: '48567777852',
+    name: name || 'Luiz Claudio Dias dos Santos Filho',
+    city: 'SANTOS',
+    amount: parseFloat(amount) || 0,
+    message: message || 'Pedido via site'
+  });
+
+  const buffer = await qrCodePix.toImageBuffer();
+  const base64 = buffer.toString('base64');
+
+  res.status(200).json({
+    qrCode: `data:image/png;base64,${base64}`
+  });
+}
