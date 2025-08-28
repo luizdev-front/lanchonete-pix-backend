741bc40e8045748d944a9d678d9ab3de0c45cc74
Luiz Claudio- Dev <luizclaudio5423@gmail.com>
2025-08-27 21:30:31 -0300

    Update create-pix.js
    Commit

--- api/create-pix.js
+++ api/create-pix.js
@@ -1,8 +1,4 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-  res.status(200).json({
-    message: 'Função está funcionando!'
-  });
+  res.status(200).json({ message: 'Função está funcionando!' });
 };
